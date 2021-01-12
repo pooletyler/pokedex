5471e96d0479954f1e5fb79ba032351f4a73dd9f
Tyler Poole <tylerpoole@Tylers-Mac-mini.local>
2021-01-12 10:45:53 -0500

    Updates

---
+++ precache-manifest.528ae745defa639f7e2104e94f00858d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0225098362d19649abb279f0f68fdb46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61542a22edca39d507d8",
+    "url": "/static/css/main.ebbc63b1.chunk.css"
+  },
+  {
+    "revision": "9ec1322b390dfd08b568",
+    "url": "/static/js/2.131847cc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.131847cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61542a22edca39d507d8",
+    "url": "/static/js/main.806d1082.chunk.js"
+  },
+  {
+    "revision": "250103716dfc89e5abb8",
+    "url": "/static/js/runtime-main.9ff9079d.js"
+  }
+]);
